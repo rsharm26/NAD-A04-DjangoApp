14f0bfa55236ca185654d6afca05c1c4c82776bf
Rohin Sharma <rsharma1340@conestogac.on.ca>
2024-04-04 12:14:37 -0400

    Added ajax call for updating homepage div with post title + body. Homepage content is now dynamically populated instead of statitcally set.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log("Hello, world")
 
 const helloWorldDiv = document.getElementById("hello-world")
+const postsBox = document.getElementById('posts-box')
 
 $.ajax({
     type: 'GET',
@@ -9,6 +10,25 @@ $.ajax({
     success: function(response) {
         console.log('success', response.text)
         helloWorldDiv.textContent = response.text
+    },
+    error: function(error) {
+        console.log("error", error)
+    }
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+
+    success: function(response) {
+        console.log('success', response.data)
+
+        response.data.forEach(element => {
+            postsBox.innerHTML += `
+                ${element.title} - <b>${element.body}</b><br>
+            `
+        });
     },
     error: function(error) {
         console.log("error", error)
--- posts/views.py
+++ posts/views.py
@@ -7,5 +7,11 @@ def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
+def load_post_data_view(request):
+    qs = Post.objects.all()
+    data = [{'id': obj.id, 'title': obj.title, 'body': obj.body, 'author': obj.author.user.username} for obj in qs]
+
+    return JsonResponse({'data': data})
+
 def hello_world_view(request):
     return JsonResponse({'text': 'Hello, world'})
