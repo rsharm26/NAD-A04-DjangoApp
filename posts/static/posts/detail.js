f2c882b4b46bd4261444bcf0eee3d6dd5deccf1a
Rohin Sharma <rsharma1340@conestogac.on.ca>
2024-04-04 16:48:43 -0400

    Populated title/body details when viewing post, added modal windows for update / delete, update features title/body information pre-populated.

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -4,7 +4,11 @@ const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 
+const postBox = document.getElementById('post-box')
 const spinnerBox = document.getElementById('spinner-box')
+
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
 
 backBtn.addEventListener('click', () => {
     history.back()
@@ -24,6 +28,20 @@ $.ajax({
             deleteBtn.classList.remove('not-visible')
         }
 
+        const titleElement = document.createElement('h3')
+        const bodyElement = document.createElement('p')
+
+        titleElement.setAttribute('class', 'mt-1')
+        titleElement.setAttribute('class', 'mt-3')
+
+        titleElement.textContent = data.title
+        bodyElement.textContent = data.body
+
+        postBox.appendChild(titleElement)
+        postBox.appendChild(bodyElement)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
         spinnerBox.classList.add('not-visible')
     },
 
