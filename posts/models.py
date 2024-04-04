419af6a02048f023f5efe1005c27a0e17902a9f5
Rohin Sharma <rsharma1340@conestogac.on.ca>
2024-04-04 11:03:12 -0400

    Added default avatar.png for profile avatar, setup DB models for profiles and posts.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from profiles.models import Profile
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200) # VARCHAR(n)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True) # Can have many users like a post but also none.
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE) # Must have an author profile per post (posts deleted if profile).
+    updated = models.DateTimeField(auto_now=True)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.tite)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Works similar to Room (Android), where a class models our entity.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # This reads as "one user per profile"
+    bio = models.TextField(default="No bio...") # 'blank=True' okay as well
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars')
+    updated = models.DateTimeField(auto_now=True) # Auto-populated
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Profile of the user {self.user.username}" # Assume this is .toString()
