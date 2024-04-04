b8f9311eabcdd54aee7c4a6b3596def3b3626dca
Rohin Sharma <rsharma1340@conestogac.on.ca>
2024-04-04 12:46:08 -0400

    Removed hello world mock, added button and logic to load more posts + display text and hide button if no more posts exits.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,57 +1,66 @@
-console.log("Hello, world")
-
-const helloWorldDiv = document.getElementById("hello-world")
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
+// Basically the number of visible posts at a time (next n posts to display)
+let visible = 3
 
-    success: function(response) {
-        console.log('success', response.text)
-        helloWorldDiv.textContent = response.text
-    },
-    error: function(error) {
-        console.log("error", error)
-    }
+// Executes per button click.
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`, // Update visible per click of "load more"
+    
+        success: function(response) {
+            console.log('success', response.data)
+            setTimeout(() => {
+                spinnerBox.classList.add('not-visible')
+    
+                response.data.forEach(element => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${element.title}</h5>
+                                <p class="card-text">${element.body}</p>
+                            </div>
+    
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+    
+                                    <div class="col-2">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                    `
+                });
+            }, 500)
+
+            if (response.size === 0) {
+                endBox.textContent = "No posts found..."
+            }
+
+            if (response.size <= visible) {
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = 'No more posts to load...'
+            }
+        },
+        error: function(error) {
+            console.log("error", error)
+        }
+    })
+}
+
+
+loadBtn.addEventListener('click', () => {
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
 
-
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-
-    success: function(response) {
-        console.log('success', response.data)
-        setTimeout(() => {
-            spinnerBox.classList.add('not-visible')
-
-            response.data.forEach(element => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${element.title}</h5>
-                            <p class="card-text">${element.body}</p>
-                        </div>
-
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-2">
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-
-                                <div class="col-2">
-                                    <a href="#" class="btn btn-primary">Like</a>
-                                </div>
-                            </div> 
-                        </div>
-                    </div>
-                `
-            });
-        }, 1000)
-    },
-    error: function(error) {
-        console.log("error", error)
-    }
-})
+getData()
--- posts/views.py
+++ posts/views.py
@@ -7,11 +7,24 @@ def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
-def load_post_data_view(request):
-    qs = Post.objects.all()
-    data = [{'id': obj.id, 'title': obj.title, 'body': obj.body, 'author': obj.author.user.username} for obj in qs]
+def load_post_data_view(request, **kwargs):
+    # Basically, get data from posts[lower to upper], so we want to retrieve n number of posts for display.
+    num_posts = kwargs.get('num_posts')
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
 
-    return JsonResponse({'data': data})
+    qs = Post.objects.all()
+    size = Post.objects.all().count()
+    data = [{
+            'id': obj.id, 
+            'title': obj.title, 
+            'body': obj.body, 
+            'liked': request.user in obj.liked.all(),
+            'author': obj.author.user.username
+        } for obj in qs]
+
+    return JsonResponse({'data': data[lower:upper], 'size': size})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'Hello, world'})
