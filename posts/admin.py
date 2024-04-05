93698e4812ed24c87d14db130a77b45bf6a80733
Rohin Sharma <rsharma1340@conestogac.on.ca>
2024-04-05 09:43:07 -0400

    Added photo carousel and some sample images.

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Photo
 # Register your models here.
 
 admin.site.register(Post) # See all posts.
+admin.site.register(Photo)
